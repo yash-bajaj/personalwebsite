18da162206d45a8ce156cb3fd510edbddc11b5ce
yash <yash@192.168.1.4>
2022-06-16 01:39:33 +0530

    some-changes

--- src/components/sections/aboutsection/AboutSection.jsx
+++ src/components/sections/aboutsection/AboutSection.jsx
@@ -7,7 +7,7 @@ function AboutSection() {
     return(
         <div className="aboutsection">   
             <h3 className="greentext">About Yash Bajaj</h3>
-            <p>Owing to my particular interest in coding, I've engaged myself in various courses, which helped me gain multi-dimensional knowledge in this specific field. I'm a punctual and hardworking 3rd-year computer engineering student and currently looking for an opportunity for personal and professional growth..</p>
+            <p>Owing to my particular interest in coding, I've engaged myself in various courses, which helped me gain multi-dimensional knowledge in this specific field. I'm a punctual and hardworking 3rd-year computer engineering student and currently looking for an opportunity for personal and professional growth.</p>
         </div>
     );
 }
--- src/components/sections/bestworksection/BestWorkSection.jsx
+++ src/components/sections/bestworksection/BestWorkSection.jsx
@@ -12,11 +12,14 @@ import dicee from "./dicee.png";
 function BestWorkSection() {
     return(
         <div className="bestworksection">
-        <h1>My Best Work</h1>
+        <h1 className="mybestwork">My Best Work</h1>
         <Row xs={1} sm={1} md={1} lg={3} className="row">
       <Work title="Keeper App" description="Google keep like clone in react." image={keeper}/>
+      <br/>
       <Work title="yech.tech" description="Personal blog website." image={yech}/>
+      <br/>
       <Work title="Dicee game" description="Roll dice and see who wins." image={dicee}/>
+      <br/>
         </Row>
         </div>
     );
